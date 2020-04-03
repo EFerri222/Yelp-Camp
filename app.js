4bf2608fdd3c35aa46da2f7ee6e2a510c893d6ef
EFerri222 <eferri222@gmail.com>
2020-04-03 15:23:54 -0400

    Set up MongoDB and Mongoose

--- app.js
+++ app.js
@@ -1,33 +1,51 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost:27017/YelpCamp', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-var campgrounds = [
-    {name: "Night at the Teebury", image: "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402_960_720.jpg"},
-    {name: "Burn Baby, Burn", image: "https://cdn.pixabay.com/photo/2016/11/21/16/03/campfire-1846142_960_720.jpg"},
-    {name: "Hippie Escape", image: "https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg"},
-    {name: "Victims of Jason", image: "https://cdn.pixabay.com/photo/2017/08/06/18/33/barn-2594975_960_720.jpg"},
-    {name: "Aurora Borealis: At This Time of Year, at This Time of Day, in This Part of the Country, Localized Entirely Within Your Campground", image: "https://cdn.pixabay.com/photo/2020/01/11/07/39/north-4756774_960_720.jpg"}
-]
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", (req,res) => {
     res.render("home");
 });
 
 app.get("/campgrounds", (req,res) => {
-    res.render("campgrounds", {campgrounds: campgrounds});
+    // Retrieve all campgrounds from DB
+    Campground.find({}, function(err,campgrounds) {
+        if(err) {
+            console.log(err);
+        } else {
+            // Render them to campgrounds page
+            res.render("campgrounds", {campgrounds: campgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", (req,res) => {
+    // Grab data from form
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    // Create new campground and save to DB
+    Campground.create(newCampground, function(err,campground) {
+        if(err) {
+            console.log(err);
+        } else {
+            // Redirect to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", (req,res) => {
