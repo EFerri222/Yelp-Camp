82a93a5559a076c0a7848984d17d5694b8b90b08
EFerri222 <eferri222@gmail.com>
2020-04-05 16:48:21 -0400

    Add Show Route

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use(express.static("public"));
 
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -20,36 +21,54 @@ app.get("/", (req,res) => {
     res.render("home");
 });
 
-app.get("/campgrounds", (req,res) => {
+// INDEX - show all campgrounds
+app.get("/index", (req,res) => {
     // Retrieve all campgrounds from DB
     Campground.find({}, function(err,campgrounds) {
         if(err) {
             console.log(err);
         } else {
             // Render them to campgrounds page
-            res.render("campgrounds", {campgrounds: campgrounds});
+            res.render("index", {campgrounds: campgrounds});
         }
     });
 });
 
+// CREATE - add new campground to db
 app.post("/campgrounds", (req,res) => {
     // Grab data from form
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
+    var description = req.body.description;
+    var newCampground = {name: name, image: image, description: description};
     // Create new campground and save to DB
     Campground.create(newCampground, function(err,campground) {
         if(err) {
             console.log(err);
         } else {
             // Redirect to campgrounds page
-            res.redirect("/campgrounds");
+            res.redirect("/index");
         }
     });
 });
 
+// NEW - show form to create new campground
 app.get("/campgrounds/new", (req,res) => {
     res.render("new");
+});
+
+// SHOW - shows more info about one campground
+app.get("/campgrounds/:id", (req,res) => {
+    // Find campground with provided id
+    var id = req.params.id;
+    Campground.find({_id:id}, function(err,campground) {
+        if(err) {
+            throw err;
+        } else {
+            // Render show template with that campground
+            res.render("show", {campground: campground[0]});
+        }
+    });
 });
 
 app.get("/*", (req,res) => {
